7a7e3f6211bff1e949954d0f14d83610289fe607
Sujith Vishwajith <sujithv28@gmail.com>
2017-04-09 22:01:44 -0400

    Updated to working VGG Model

--- model.py
+++ model.py
@@ -8,13 +8,19 @@ import tensorflow as tf
 tf.python.control_flow_ops = tf
 
 from keras.models import Sequential
+from keras.applications.vgg16 import VGG16,preprocess_input
 from keras.layers.core import Flatten, Dense, Dropout, Lambda
+from keras.layers import Input
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
-from PIL import Image
+from keras.models import Model
 from keras.utils import np_utils
+from PIL import Image
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
+from scipy import stats, integrate
+from skimage import io, img_as_float
 
 # GLOBAL CONSTANTS
 DATA_FILE = 'leafsnap-dataset-images.csv'
@@ -30,9 +36,12 @@ print('\n[INFO] Creating Training and Testing Data (75-25 Split):')
 images_train, images_validation, species_train, species_validation = train_test_split(
     data['image_pat'], data['species'], test_size=0.25, random_state=42)
 
+encoder = LabelEncoder()
+encoder.fit(species_train)
+species_train = encoder.transform(species_train)
 species_train = np_utils.to_categorical(species_train)
+species_validation = encoder.transform(species_validation)
 species_validation = np_utils.to_categorical(species_validation)
-num_classes = species_validation.shape[1]
 
 # Rotates an image 90 degrees
 def check_and_rotate(image, angle):
@@ -50,6 +59,24 @@ def check_and_rotate(image, angle):
     else:
         return image
 
+def load_image_and_preprocess(path, flip_left_right=False, flip_up_down=False, rotate_180=False):
+    # Open image from disk and flip it if generating data.
+    image = Image.open(path.strip())
+
+    if flip_left_right:
+        image = image.transpose(Image.FLIP_LEFT_RIGHT)
+    if flip_up_down:
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    if rotate_180:
+        image = image.transpose(Image.ROTATE_180)
+
+    # Convert the image into mulitdimensional matrix of float values (normally int which messes up our division).
+    image = np.array(image, np.float32)
+    # Resize Image
+    image = scipy.misc.imresize(image, (224,224))
+    # Return the modified image.
+    return image
+
 print('\n[INFO] Batch Generator:')
 def batch_generator(images, species, batch_size=64, augment_data=True):
     # Create an array of sample indices.
@@ -63,14 +90,9 @@ def batch_generator(images, species, batch_size=64, augment_data=True):
         np.random.shuffle(indices)
         for i in indices:
             path = images.iloc[i]
-            original_image = Image.open(path.strip())
-            original_image = np.array(original_image, np.float32)
-            # original_image = check_and_rotate(original_image, 90)
-
-            # Resize all the images to the same size
-            original_image = scipy.misc.imresize(original_image, (224,224))
             original_species = species[i]
 
+            image = load_image_and_preprocess(path)
             batch_images.append(original_image)
             batch_species.append(original_species)
 
@@ -78,15 +100,15 @@ def batch_generator(images, species, batch_size=64, augment_data=True):
             # images and we want as much data as possible.
             if augment_data:
                 # Flip the image left right, up down, and rotated 180 to augment data
-                flipped_left_right_image = original_image.transpose(Image.FLIP_LEFT_RIGHT)
+                flipped_left_right_image = load_image_and_preprocess(path,flip_left_right=True)
                 batch_images.append(flipped_left_right_image)
                 batch_species.append(original_species)
 
-                flipped_up_down_image = original_image.transpose(Image.FLIP_TOP_BOTTOM)
+                flipped_up_down_image = load_image_and_preprocess(path,flip_up_down=True)
                 batch_images.append(flipped_up_down_image)
                 batch_species.append(original_species)
 
-                rotated_180_image = original_image.transpose(Image.ROTATE_180)
+                rotated_180_image = load_image_and_preprocess(path,rotate_180=True)
                 batch_images.append(rotated_180_image)
                 batch_species.append(original_species)
 
@@ -102,84 +124,51 @@ def batch_generator(images, species, batch_size=64, augment_data=True):
                 batch_images = []
                 batch_species = []
 
-def VGG_16(weights_path=None):
-    model = Sequential()
-    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=(224,224,3)))
+def VGG_16():
+    base_model = VGG16(weights='imagenet', include_top=False)
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    for layer in base_model.layers:
+        layer.trainable = False
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    input = Input(shape=(224,224,3),name = 'image_input')
+    output_vgg16_conv = base_model(input)
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    x = Flatten(name='flatten')(output_vgg16_conv)
+    x = Dense(4096, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(2048, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(NUM_CLASSES, activation='softmax')(x)
 
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2,2), strides=(2,2)))
-
-    model.add(Flatten())
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(2048, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(NUM_CLASSES, activation='softmax'))
-
-    if weights_path:
-        model.load_weights(weights_path)
-
+    model = Model(input=input, output=x)
     return model
 
 print('\n[INFO] Generating training and validation batches')
-samples_per_epoch = 4 * len(images_train)
+steps_per_epoch = 4 * len(images_train)
 generator_train = batch_generator(images_train, species_train, augment_data=True)
-nb_val_samples = len(images_validation)
-X_validation,y_validation = batch_generator(images_validation, species_validation, augment_data=False)
+validation_steps = len(images_validation)
+generator_validation = batch_generator(images_validation, species_validation, augment_data=False)
 
 print('\n[INFO] Creating Model:')
-model = VGG_16(VGG_WEIGHTS_FILE)
+model = VGG_16()
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(optimizer=sgd, loss='categorical_crossentropy')
 
 print('\n[INFO] Training Model:')
-model.fit_generator(generator_train,
-                    samples_per_epoch=samples_per_epoch,
-                    nb_epoch=NB_EPOCH,
-                    validation_data=(X_validation,y_validation),
-                    nb_val_samples=nb_val_samples)
+history = model.fit_generator(generator_train,
+                              steps_per_epoch=steps_per_epoch,
+                              nb_epoch=NB_EPOCH,
+                              validation_data=generator_validation,
+                              validation_steps=validation_steps)
 
-model.save_weights('model.h5', True)
-with open('model.json', 'w') as outfile:
+model.save_weights('vgg_model.h5', True)
+with open('vgg_model.json', 'w') as outfile:
     json.dump(model.to_json(), outfile)
 
-score = model.evaluate(X_validation, y_validation, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+# score = model.evaluate(X_validation, y_validation, verbose=0)
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
