2b7d650d09c5cb2ad77efcc48ea6b8c0d43b5bdc
Sujith Vishwajith <sujithv28@gmail.com>
2017-05-10 14:02:03 -0400

    fix typo

--- test.py
+++ test.py
@@ -68,7 +68,7 @@ def test(test_loader, model, criterion):
 
         # measure accuracy and record loss
         prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
-        _, predicted = torch.max(outputs.data, 1)
+        _, predicted = torch.max(output.data, 1)
         print('Predicted: ', ' '.join('%5s' % classes[predicted[0][0]]))
         losses.update(loss.data[0], input.size(0))
         top1.update(prec1[0], input.size(0))
