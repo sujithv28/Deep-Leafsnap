d78fb76aec1fd3b1f41dc148c06b8f53357ff17c
Sujith Vishwajith <sujithv28@gmail.com>
2017-05-10 12:42:23 -0400

    Files

--- test.py
+++ test.py
@@ -29,10 +29,75 @@ from torchvision import transforms
 
 # GLOBAL CONSTANTS
 INPUT_SIZE = 224
-classes = []
+NUM_CLASSES = 185
+NUM_EPOCHS = 35
+LEARNING_RATE = 1e-1
 USE_CUDA = torch.cuda.is_available()
+best_prec1 = 0
+classes = []
 
-def test(test_loader, model, criterion):
+# ARGS Parser
+parser = argparse.ArgumentParser(description='PyTorch LeafSnap Training')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+args = parser.parse_args()
+
+# Training method which trains model for 1 epoch
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (input, target) in enumerate(train_loader):
+        # measure data loading time
+        if USE_CUDA:
+            input = input.cuda(async=True)
+            target = target.cuda(async=True)
+
+        data_time.update(time.time() - end)
+
+        input_var = torch.autograd.Variable(input)
+        target_var = torch.autograd.Variable(target)
+
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        losses.update(loss.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % 100 == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  '\Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                      epoch, i, len(train_loader), batch_time=batch_time,
+                      data_time=data_time, loss=losses, top1=top1, top5=top5))
+
+# Validation method
+def validate(val_loader, model, criterion):
     batch_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
@@ -44,7 +109,7 @@ def test(test_loader, model, criterion):
     model.eval()
 
     end = time.time()
-    for i, (input, target) in enumerate(test_loader):
+    for i, (input, target) in enumerate(val_loader):
         if USE_CUDA:
             input = input.cuda(async=True)
             target = target.cuda(async=True)
@@ -79,6 +144,24 @@ def test(test_loader, model, criterion):
 
     return top1.avg
 
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        print('\n[INFO] Saved Model to model_best.pth.tar')
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = LEARNING_RATE * (0.1 ** (epoch // 6))
+    if (lr <= 0.0001):
+        lr = 0.0001
+    print('\n[Learning Rate] {:0.6f}'.format(lr))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
 def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
     maxk = max(topk)
@@ -94,7 +177,32 @@ def accuracy(output, target, topk=(1,)):
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
 
-print('\n[INFO] Reading Testing Dataset')
+print('\n[INFO] Creating Model')
+model = torch.load('model_best.pth.tar')
+
+print('\n[INFO] Model Architecture: \n{}'.format(model))
+
+criterion = nn.CrossEntropyLoss()
+if USE_CUDA:
+    model = torch.nn.DataParallel(model).cuda()
+    criterion = criterion.cuda()
+optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE,
+                      momentum=0.9, weight_decay=1e-4, nesterov=True)
+
+if args.resume:
+    if os.path.isfile(args.resume):
+        print("=> loading checkpoint '{}'".format(args.resume))
+        checkpoint = torch.load(args.resume)
+        args.start_epoch = checkpoint['epoch']
+        best_prec1 = checkpoint['best_prec1']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        print("=> loaded checkpoint '{}' (epoch {})"
+              .format(args.resume, checkpoint['epoch']))
+    else:
+        print("=> no checkpoint found at '{}'".format(args.resume))
+
+print('\n[INFO] Reading Training and Testing Dataset')
 traindir = os.path.join('dataset', 'train')
 testdir = os.path.join('dataset', 'test')
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -108,16 +216,13 @@ data_test = datasets.ImageFolder(testdir, transforms.Compose([
     normalize]))
 classes = data_train.classes
 
-test_loader = torch.utils.data.DataLoader(data_test, batch_size=64, shuffle=False, num_workers=2)
-
-criterion = nn.CrossEntropyLoss()
-if USE_CUDA:
-	criterion = criterion.cuda()
-model = torch.load('model_best.pth.tar')
-print('\n[INFO] Model Architecture: \n{}'.format(model))
+train_loader = torch.utils.data.DataLoader(data_train, batch_size=64, shuffle=True, num_workers=2)
+val_loader = torch.utils.data.DataLoader(data_test, batch_size=64, shuffle=False, num_workers=2)
 
 print('\n[INFO] Testing Started')
-prec1 = test(test_loader, model, criterion)
-print(prec1)
+for epoch in range(1, 2):
+    adjust_learning_rate(optimizer, epoch)
+    prec1 = validate(val_loader, model, criterion)
+    print(prec1)
 
 print('\n[DONE]')
